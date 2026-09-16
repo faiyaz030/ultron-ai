056d0fe216e419181a0fb3a67e11690a8b5343b9
faiyaz030 <alfaiyaz30@gmail.com>
2026-09-16 09:43:54 +0600

    app.ts

---
+++ Api server/Api server/src/app.ts
@@ -0,0 +1,36 @@
+import express, { type Express } from "express";
+import cors from "cors";
+import pinoHttp from "pino-http";
+import router from "./routes";
+import { logger } from "./lib/logger";
+import { applySecurityMiddleware } from "./middlewares/security";
+
+const app: Express = express();
+
+app.use(
+  pinoHttp({
+    logger,
+    serializers: {
+      req(req) {
+        return {
+          id: req.id,
+          method: req.method,
+          url: req.url?.split("?")[0],
+        };
+      },
+      res(res) {
+        return {
+          statusCode: res.statusCode,
+        };
+      },
+    },
+  }),
+);
+
+applySecurityMiddleware(app);
+app.use(express.json({ limit: "64kb" }));
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/api", router);
+
+export default app;
